6b6b39247448d1d4fd13d1d96692c40ee167bd27
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-07-22 13:39:52 +0300

    Adding randomness logic to Person model

--- PhoneBook/Person.swift
+++ PhoneBook/Person.swift
@@ -20,6 +20,32 @@ struct Person {
 }
 
 extension Person {
-    
+    static func getContactList() -> [Person] {
+        var persons: [Person] = []
+        
+        let fistNames = DataStore.shared.fistNameList.shuffled()
+        let lastNames = DataStore.shared.lastNameList.shuffled()
+        let emails = DataStore.shared.email.shuffled()
+        let phones = DataStore.shared.phoneNumber.shuffled()
+        
+        let interationCount = min(
+            fistNames.count,
+            lastNames.count,
+            emails.count,
+            phones.count
+        )
+        
+        for index in 0..<interationCount {
+            let person = Person(
+                fistName: fistNames[index],
+                lastName: lastNames[index],
+                email: emails[index],
+                phone: phones[index]
+            )
+            persons.append(person)
+        }
+        
+        return persons
+    }
 }
 
